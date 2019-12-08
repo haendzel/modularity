fe2498ffdefd527f77f9c0aee6b3d80358bf44d1
Filip Handzel <f.handzel@gmail.com>
2019-12-08 16:21:56 +0100

    php files wp

---
+++ footer.php
@@ -0,0 +1,24 @@
+<div class="wrapper">
+        <footer> <!-- FOOTER -->
+            <div class="metadata">
+                <a href="#" class="logo"><img src="/images/logo.svg"></a>
+                    <a href="#one">Point One</a>
+                    <a href="#two">Point Two</a>
+                    <a href="#three">Point Three</a>
+                    <a href="#four">Point Four</a>
+                    <a href="#five">Point Five</a>
+                <div class="copyright">© 2019 COMPANY. All rights reserved.</div>
+                <div class="social">
+                    <a href="http://facebook.com"><img src="images/fb.png"></a>
+                    <a href="http://linkedin.com"><img src="images/in.png"></a>
+                </div>
+            </div>
+        </footer>
+    </div> <!-- wrapper -->
+
+</div> <!-- container-->
+
+<script src="js/main.min.js"></script>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ src/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php wp_head(); ?>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php bloginfo('name'); ?></title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+
+<div class="container">
+    
+    <header> <!-- HEADER NAV -->
+        <a href="#" class="logo"><img src="/images/logo.svg"></a>
+            <ul>
+                <li><a href="#product">Product</a></li>
+                <li><a href="#partners">Partners</a></li>
+                <li><a href="#features">Features</a></li>
+                <li><a href="#team">Team</a></li>
+                <li><a href="#about">About us</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+    </header>
